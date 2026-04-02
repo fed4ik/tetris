452a8c20c5a6340e88e3ee4a1f4325d1dfa09fef
o_fed4ik <fed4iksasha@gmail.com>
2026-04-02 15:05:54 +0300

    commit 2

---
+++ src/main/java/tetris/Shape.java
@@ -0,0 +1,66 @@
+package tetris;
+
+public class Shape {
+    public enum Tetrominoes { NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape }
+    private Tetrominoes pieceShape;
+    private int[] coords = new int[4 * 2]; // x,y for 4 blocks
+
+    private static final int[][][] coordsTable = {
+        {{0,0}, {0,0}, {0,0}, {0,0}}, // NoShape
+        {{0,-1}, {0,0}, {-1,0}, {-1,1}}, // ZShape
+        {{0,-1}, {0,0}, {1,0}, {1,1}}, // SShape
+        {{0,-1}, {0,0}, {0,1}, {0,2}}, // LineShape
+        {{-1,0}, {0,0}, {1,0}, {0,1}}, // TShape
+        {{0,0}, {1,0}, {0,1}, {1,1}}, // SquareShape
+        {{-1,-1}, {0,-1}, {0,0}, {0,1}}, // LShape
+        {{1,-1}, {0,-1}, {0,0}, {0,1}} // MirroredLShape
+    };
+
+    public Shape() {
+        setShape(Tetrominoes.NoShape);
+    }
+
+    public void setShape(Tetrominoes shape) {
+        for (int i = 0; i < 4; i++) {
+            coords[2*i] = coordsTable[shape.ordinal()][i][0];
+            coords[2*i+1] = coordsTable[shape.ordinal()][i][1];
+        }
+        pieceShape = shape;
+    }
+
+    public void setRandomShape() {
+        int x = (int)(Math.random() * 7) + 1;
+        Tetrominoes[] values = Tetrominoes.values();
+        setShape(values[x]);
+    }
+
+    public int x(int index) { return coords[2*index]; }
+    public int y(int index) { return coords[2*index+1]; }
+    public Tetrominoes getShape() { return pieceShape; }
+
+    public Shape rotateLeft() {
+        if (pieceShape == Tetrominoes.SquareShape) return this;
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+        for (int i = 0; i < 4; ++i) {
+            int x = y(i);
+            int y = -x(i);
+            result.coords[2*i] = x;
+            result.coords[2*i+1] = y;
+        }
+        return result;
+    }
+
+    public Shape rotateRight() {
+        if (pieceShape == Tetrominoes.SquareShape) return this;
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+        for (int i = 0; i < 4; ++i) {
+            int x = -y(i);
+            int y = x(i);
+            result.coords[2*i] = x;
+            result.coords[2*i+1] = y;
+        }
+        return result;
+    }
+}
